a2c2e8f5c0387aac57753330be81eff2292f2bb1
TcThinh <tcthinh10061998@gmail.com>
2024-06-13 08:56:48 +0700

    initialize

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import CSVUploader from "./components/CSVUploader";
+
+function App() {
+  return (
+    <div>
+      <CSVUploader />
+    </div>
+  );
+}
+
+export default App;
